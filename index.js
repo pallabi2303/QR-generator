4f5fc8cc1d53c7dc20ea8c24518f22b32e03364b
Pallabi Pati <116326163+pallabi2303@users.noreply.github.com>
2024-07-20 19:40:07 +0530

    Update index.js
    used inquirer and qr-image npm for qr generation

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 import inquirer from "inquirer";
 import x from "qr-image";
 import fs from "fs";
+inquirer
+  .prompt([{
+    message:"Type your url -> ",
+   name:"URL"
+  }])
+  .then((answers) => {
+    const url=answers.URL;
+    var qr_svg = x.image(url, { type: 'png' });
+    qr_svg.pipe(fs.createWriteStream('url-qr.png'));
+
+    fs.writeFile("url.txt",url,(err)=>{
+        if(err) throw err;
+        console.log("Successfully saved");
+  });
+})
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
